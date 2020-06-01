811b8fa23750498af194d8c90d8f33548de1092d
darkfusion90 <gauravsharmaa341@gmail.com>
2020-06-01 11:57:33 +0530

    Added action: fetchPublicRooms

---
+++ lib/redux/actions/public_rooms_actions/fetch_public_rooms.dart
@@ -0,0 +1,26 @@
+import 'package:chatrooms/dummy_data_provider/RoomProvider.dart';
+import 'package:chatrooms/redux/models/room.dart';
+import 'package:chatrooms/redux/state.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:redux/src/store.dart';
+import 'package:redux_thunk/redux_thunk.dart';
+
+ThunkAction<AppState> fetchPublicRooms(BuildContext context) {
+  print('ThunkAction dispatch: fetchPublicRooms');
+  return (Store<AppState> store) async {
+    print('Async fetchPublicRooms started');
+    try {
+      final List<Room> rooms = await RoomsProvider.rooms(context);
+      print('Async fetchPublicRooms succeeded: $rooms');
+      store.dispatch(FetchPublicRooms(rooms));
+    } catch (on, stackTrace) {
+      print('Error fetching rooms: $on\nStack Trace: $stackTrace');
+    }
+  };
+}
+
+class FetchPublicRooms {
+  final List<Room> rooms;
+
+  const FetchPublicRooms(this.rooms);
+}
