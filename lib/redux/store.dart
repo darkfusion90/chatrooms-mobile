9327d4a60ca90d59f2f695329b93d06239e63787
darkfusion90 <gauravsharmaa341@gmail.com>
2020-06-01 11:53:30 +0530

    Added redux-thunk middleware

--- lib/redux/store.dart
+++ lib/redux/store.dart
@@ -1,8 +1,10 @@
 import 'package:chatrooms/redux/reducers/main.dart';
 import 'package:chatrooms/redux/state.dart';
 import 'package:redux/redux.dart';
+import 'package:redux_thunk/redux_thunk.dart';
 
 final Store<AppState> appStore = Store<AppState>(
   appReducer,
   initialState: AppState.init(),
+  middleware: [thunkMiddleware],
 );
