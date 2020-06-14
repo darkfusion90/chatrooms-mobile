20d24f629dccbdbfeb286173fef9381a85fb6bf4
darkfusion90 <gauravsharmaa341@gmail.com>
2020-06-14 14:32:34 +0530

    Added AppBar for screen "Room"

---
+++ lib/reusable_widgets/appbars/room_appbar/room-appbar-popup-menu.dart
@@ -0,0 +1,45 @@
+import 'package:chatrooms/reusable_widgets/IconContent.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RoomAppbarPopupMenu extends StatelessWidget {
+  final List<_PopupItemViewModel> _popupItems = [
+    _PopupItemViewModel(
+      Icons.exit_to_app,
+      'Leave Room',
+      () => print('onPressed leave room'),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<_PopupItemViewModel>(
+      itemBuilder: _buildPopupMenuItems,
+      offset: Offset(0, 32.0),
+      onSelected: (popup) => popup.onItemPressed(),
+    );
+  }
+
+  List<PopupMenuEntry<_PopupItemViewModel>> _buildPopupMenuItems(_) =>
+      _popupItems.map((_PopupItemViewModel popup) => popup.build()).toList();
+}
+
+class _PopupItemViewModel {
+  final IconData icon;
+  final String text;
+  final VoidCallback onItemPressed;
+
+  _PopupItemViewModel(this.icon, this.text, this.onItemPressed);
+
+  PopupMenuEntry<_PopupItemViewModel> build() =>
+      PopupMenuItem<_PopupItemViewModel>(
+        value: this,
+        child: IconContent(
+          icon: Icon(icon, color: Colors.black),
+          content: Text(text),
+        ),
+      );
+
+  @override
+  String toString() => 'PopupItem -> text: $text';
+}
