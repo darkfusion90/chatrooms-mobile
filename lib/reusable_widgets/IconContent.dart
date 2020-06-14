2686bd817d56ea0305ff123c99a252e10bfc4b1f
darkfusion90 <gauravsharmaa341@gmail.com>
2020-06-14 14:54:13 +0530

    Widget added to show icon along with other content

---
+++ lib/reusable_widgets/IconContent.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class IconContent extends StatelessWidget {
+  final IconPosition iconPosition;
+  final Icon icon;
+  final Widget content;
+
+  const IconContent({
+    @required this.icon,
+    @required this.content,
+    this.iconPosition = IconPosition.leading,
+  })  : assert(iconPosition != null),
+        assert(icon != null),
+        assert(content != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: _buildChildren(),
+      ),
+    );
+  }
+
+  List<Widget> _buildChildren() {
+    return iconPosition == IconPosition.leading
+        ? [icon, content]
+        : [content, icon];
+  }
+}
+
+enum IconPosition { leading, trailing }
