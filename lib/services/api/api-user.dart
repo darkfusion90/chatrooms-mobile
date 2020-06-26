6175d9bdb269e408e2fbc8f1be55fe0b1a227715
darkfusion90 <gauravsharmaa341@gmail.com>
2020-06-26 12:18:20 +0530

    Added 'login', 'logout' and 'register' api methods.

--- lib/services/api/api-user.dart
+++ lib/services/api/api-user.dart
@@ -1,11 +1,22 @@
+import 'package:requests/requests.dart';
+
 import 'package:chatrooms/redux/models/account.dart';
 import 'package:chatrooms/services/api/api.dart';
 import 'package:chatrooms/services/api/config.dart';
 
 class ApiUser {
   static final String _root = ApiConfig.createUrl('user');
+  static final String _loginRoot = ApiConfig.createUrl('login');
+  static final String _logoutRoot = ApiConfig.createUrl('logout');
+  static final String _registerRoot = ApiConfig.createUrl('registered');
 
   Future<AccountModel> getCurrentUserAccount() async {
     return AccountModel.fromJson(await Api.getJson(_root));
   }
+
+  Future<Response> login() => Requests.get(_loginRoot);
+
+  Future<Response> logout() => Requests.get(_logoutRoot);
+
+  Future<Response> register() => Requests.get(_registerRoot);
 }
