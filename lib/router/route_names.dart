5fe109886ca3b393a5d0b922cb5d93b02635cc2c
darkfusion90 <gauravsharmaa341@gmail.com>
2020-06-27 18:50:31 +0530

    Added router with named routes

---
+++ lib/router/route_names.dart
@@ -0,0 +1,6 @@
+abstract class RouteNames {
+  static const String home = 'home';
+  static const String room = 'room';
+  static const String signIn = 'sign-in';
+  static const String createAccount = 'create-account';
+}
---
+++ lib/router/router.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:chatrooms/router/route_names.dart';
+import 'package:chatrooms/screens/create_account/create-account-screen.dart';
+import 'package:chatrooms/screens/home/home_screen.dart';
+import 'package:chatrooms/screens/room/room_screen.dart';
+import 'package:chatrooms/screens/sign_in/sign_in_screen.dart';
+
+abstract class Router {
+  static MaterialPageRoute get _home =>
+      MaterialPageRoute(builder: (_) => Home());
+
+  static MaterialPageRoute get _room =>
+      MaterialPageRoute(builder: (_) => Room());
+
+  static MaterialPageRoute get _signIn =>
+      MaterialPageRoute(builder: (_) => SignInScreen());
+
+  static MaterialPageRoute get _createAccount =>
+      MaterialPageRoute(builder: (_) => CreateAccountScreen());
+
+  static MaterialPageRoute generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RouteNames.home:
+        return _home;
+      case RouteNames.room:
+        return _room;
+      case RouteNames.signIn:
+        return _signIn;
+      case RouteNames.createAccount:
+        return _createAccount;
+      default:
+        throw RouteResolveException(settings.name);
+    }
+  }
+}
+
+class RouteResolveException implements Exception {
+  final String routeName;
+
+  String get message => 'Could not resolve route: $routeName';
+
+  RouteResolveException(this.routeName);
+}
