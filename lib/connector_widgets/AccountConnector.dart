2c68f70b54d8cc01018d38baa287f1fa0af78d86
darkfusion90 <gauravsharmaa341@gmail.com>
2020-06-27 19:08:53 +0530

    Updated AccountConnector's ViewModel to include login/logout and updateAccount along with the account details

--- lib/connector_widgets/AccountConnector.dart
+++ lib/connector_widgets/AccountConnector.dart
@@ -1,3 +1,6 @@
+import 'package:chatrooms/redux/actions/account_actions/login.dart';
+import 'package:chatrooms/redux/actions/account_actions/logout.dart';
+import 'package:chatrooms/redux/actions/account_actions/set-user-account.dart';
 import 'package:chatrooms/redux/models/account.dart';
 import 'package:chatrooms/redux/selectors/account_selectors/account_selector.dart';
 import 'package:chatrooms/redux/state/AppState.dart';
@@ -7,16 +10,49 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 
 class AccountConnector extends StatelessWidget {
-  final ParameterizedWidgetBuilder<AccountModel> builder;
+  final ParameterizedWidgetBuilder<AccountConnectorViewModel> builder;
 
   const AccountConnector({@required this.builder});
 
   @override
   Widget build(BuildContext context) {
-    return StoreConnector(
-      converter: (Store<AppState> store) => store,
-      builder: (BuildContext context, Store<AppState> store) =>
-          builder(context, accountStateSelector(store.state).account),
+    return StoreConnector<AppState, AccountConnectorViewModel>(
+      converter: _viewModel,
+      builder: (BuildContext context, AccountConnectorViewModel viewModel) =>
+          builder(context, viewModel),
     );
   }
+
+  AccountConnectorViewModel _viewModel(Store<AppState> store) {
+    return AccountConnectorViewModel(
+      account: accountSelector(store.state),
+      login: (LoginInfo loginInfo) => store.dispatch(login(loginInfo)),
+      logout: () => store.dispatch(logout()),
+      updateAccount: () => store.dispatch(updateUserAccount()),
+    );
+  }
+}
+
+class AccountConnectorViewModel {
+  final AccountModel account;
+  final ValueChanged<LoginInfo> login;
+  final VoidCallback logout;
+  final VoidCallback updateAccount;
+
+  AccountConnectorViewModel({
+    @required this.account,
+    @required this.login,
+    @required this.logout,
+    @required this.updateAccount,
+  })  : assert(account != null),
+        assert(login != null),
+        assert(logout != null),
+        assert(updateAccount != null);
+}
+
+class LoginInfo {
+  final String username;
+  final String password;
+
+  LoginInfo(this.username, this.password);
 }
