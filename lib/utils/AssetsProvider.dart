c8e818ff10369aea952488fa7b6b1fa063a1d622
darkfusion90 <gauravsharmaa341@gmail.com>
2020-06-27 19:16:16 +0530

    Added central repo for Asset keys throughout the app

---
+++ lib/utils/AssetsProvider.dart
@@ -0,0 +1,6 @@
+abstract class AssetKeyProvider {
+  static const String assets = 'assets';
+  static const String imageAssets = 'assets/images';
+  static const String guestAccountAvatar =
+      '$imageAssets/guest-account-avatar.png';
+}
