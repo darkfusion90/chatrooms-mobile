e4d2a5e4f36073dbbeed7f798244301234ad2363
darkfusion90 <gauravsharmaa341@gmail.com>
2020-06-27 19:19:43 +0530

    Action added: login

---
+++ lib/redux/actions/account_actions/login.dart
@@ -0,0 +1,18 @@
+import 'package:chatrooms/connector_widgets/AccountConnector.dart';
+import 'package:chatrooms/redux/actions/account_actions/set-user-account.dart';
+import 'package:chatrooms/redux/state/AppState.dart';
+import 'package:chatrooms/services/api/api.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_thunk/redux_thunk.dart';
+import 'package:requests/requests.dart';
+
+ThunkAction<AppState> login(LoginInfo info) {
+  return (Store<AppState> store) async {
+    print('username: ${info.username}, password: ${info.password}');
+    final Response response =
+        await Api.user.login(info.username, info.password);
+    
+    print('Response: ${response.statusCode}\n${response.json()}');
+    store.dispatch(updateUserAccount());
+  };
+}
