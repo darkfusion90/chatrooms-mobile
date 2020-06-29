84282e08a75d5d9580b4dcfeb6338ac7c4daba27
darkfusion90 <gauravsharmaa341@gmail.com>
2020-06-29 14:46:08 +0530

    Register method implemented in api. Changed register route to POST /api/user/

--- lib/services/api/api-user.dart
+++ lib/services/api/api-user.dart
@@ -1,3 +1,4 @@
+import 'package:chatrooms/connector_widgets/AccountConnector.dart';
 import 'package:requests/requests.dart';
 
 import 'package:chatrooms/redux/models/account.dart';
@@ -8,20 +9,22 @@ class ApiUser {
   static final String _root = ApiConfig.createUrl('user');
   static final String _loginRoot = ApiConfig.createUrl('login');
   static final String _logoutRoot = ApiConfig.createUrl('logout');
-  static final String _registerRoot = ApiConfig.createUrl('registered');
+  static final String _registerRoot = _root;
 
   Future<AccountModel> getCurrentUserAccount() async {
     return AccountModel.fromJson(await Api.getJson(_root));
   }
 
-  Future<Response> login(String username, String password) {
-    return Requests.post(
-      _loginRoot,
-      body: {'username': username, 'password': password},
-    );
-  }
+  Future<Response> login(LoginInfo info) => _postAccount(_loginRoot, info);
 
   Future<Response> logout() => Requests.post(_logoutRoot);
 
-  Future<Response> register() => Requests.get(_registerRoot);
+  Future<Response> register(LoginInfo info) =>
+      _postAccount(_registerRoot, info);
+
+  Future<Response> _postAccount(String url, LoginInfo info) =>
+      Requests.post(url, body: {
+        'username': info.username,
+        'password': info.password,
+      });
 }
