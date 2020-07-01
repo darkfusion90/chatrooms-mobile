1d21f7029ff4b9a04e60add929e573f380123b66
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-01 19:57:40 +0530

    Rearranged import statements to keep the related ones close to each other

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,7 @@
-import 'package:chatrooms/widgets/drawers/default_drawer/default-drawer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:chatrooms/widgets/drawers/default_drawer/default-drawer.dart';
 import 'package:chatrooms/widgets/appbars/home-appbar.dart';
 import 'package:chatrooms/screens/home/widgets/room_list/RoomList.dart';
 
