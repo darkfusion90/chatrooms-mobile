0d5b94177135042b5655c78d45f8234e4118c105
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-01 20:00:40 +0530

    Hooked AccountConnector to SignInScreen

--- lib/screens/sign_in/sign_in_screen.dart
+++ lib/screens/sign_in/sign_in_screen.dart
@@ -1,9 +1,29 @@
+import 'package:chatrooms/connector_widgets/AccountConnector.dart';
 import 'package:flutter/material.dart';
 
+import 'package:chatrooms/redux/models/account.dart';
 import 'package:chatrooms/screens/sign_in/widgets/sign_in_form.dart';
 import 'package:chatrooms/widgets/appbars/sign-in-appbar.dart';
 
 class SignInScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AccountConnector(
+      builder: (_, viewModel) => _SignInScreenView(account: viewModel.account),
+    );
+  }
+}
+
+class _SignInScreenView extends StatefulWidget {
+  final AccountModel account;
+
+  const _SignInScreenView({@required this.account}) : assert(account != null);
+
+  @override
+  _SignInScreenViewState createState() => _SignInScreenViewState();
+}
+
+class _SignInScreenViewState extends State<_SignInScreenView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
