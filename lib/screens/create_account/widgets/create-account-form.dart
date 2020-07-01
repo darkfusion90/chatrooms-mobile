7a53c4b386a6731b839cfd2da01b865f666c6c1e
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-01 19:55:29 +0530

    After onCreateAccount, user is redirected to home. Although the success/failure doesn't affect the redirection (...yet)

--- lib/screens/create_account/widgets/create-account-form.dart
+++ lib/screens/create_account/widgets/create-account-form.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/material.dart';
+
 import 'package:chatrooms/connector_widgets/AccountConnector.dart';
 import 'package:chatrooms/router/route_names.dart';
 import 'package:chatrooms/widgets/PrimaryColoredTextFlatButton.dart';
 import 'package:chatrooms/widgets/account-action-form.dart';
-import 'package:flutter/material.dart';
 
 class CreateAccountForm extends StatelessWidget {
   @override
@@ -25,7 +26,11 @@ class _CreateAccountFormView extends StatelessWidget {
   Widget build(BuildContext context) {
     return AccountActionForm(
       actionType: AccountActionType.createAccount,
-      onFormSubmit: onCreateAccount,
+      onFormSubmit: (LoginInfo info) {
+        onCreateAccount(info);
+        Navigator.of(context)
+            .pushNamedAndRemoveUntil(RouteNames.home, (route) => false);
+      },
       extraAction: PrimaryColoredTextFlatButton(
         text: 'Sign In',
         onPressed: () => Navigator.of(context).pushNamed(RouteNames.signIn),
