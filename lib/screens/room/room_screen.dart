3a18f1f4ecc03a050a2ad23dd478e2b1b04821c6
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-02 20:01:04 +0530

    Conditional rendering on different membership situations (undetermined, notMember, member)

--- lib/screens/room/room_screen.dart
+++ lib/screens/room/room_screen.dart
@@ -1,11 +1,14 @@
 import 'package:chatrooms/connector_widgets/ActiveRoomConnector.dart';
 import 'package:chatrooms/redux/models/room.dart';
+import 'package:chatrooms/screens/room/widgets/modes/room_mode_member.dart';
+import 'package:chatrooms/screens/room/widgets/modes/room_mode_not_member.dart';
 import 'package:chatrooms/widgets/appbars/room_appbar/room-appbar.dart';
 import 'package:chatrooms/screens/room/widgets/chat_view/ChatView.dart';
 import 'package:chatrooms/services/socket_io/SocketService.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Room extends StatelessWidget {
   @override
@@ -19,7 +22,7 @@ class Room extends StatelessWidget {
   }
 }
 
-class _RoomView extends StatefulWidget {
+class _RoomView extends StatelessWidget {
   final RoomModel room;
   final VoidCallback resetActiveRoom;
 
@@ -29,37 +32,25 @@ class _RoomView extends StatefulWidget {
   });
 
   @override
-  __RoomViewState createState() => __RoomViewState();
-}
-
-class __RoomViewState extends State<_RoomView> {
-  @override
-  void initState() {
-    super.initState();
-    _joinRoom();
-  }
-
-  @override
-  void didUpdateWidget(_RoomView oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.room != widget.room) {
-      _joinRoom();
-    }
-  }
-
-  @override
-  void dispose() {
-    widget.resetActiveRoom();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    if (room == null || room.isMembershipUndetermined) {
+      return _loadingIndicator();
+    }
+
+    return room.isMember ? RoomModeViewMember() : RoomModeViewNonMember();
+  }
+
+  Widget _loadingIndicator() {
     return Scaffold(
-      appBar: RoomAppBar(widget.room),
-      body: ChatView(),
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SpinKitChasingDots(color: Colors.orange),
+          SizedBox(height: 10),
+          Text('Loading', style: TextStyle(fontSize: 26, color: Colors.grey)),
+        ],
+      ),
     );
   }
-
-  void _joinRoom() => SocketService().room.joinRoom(widget.room?.id);
 }
