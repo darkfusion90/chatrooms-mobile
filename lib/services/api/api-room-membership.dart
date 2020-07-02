43d28d1dd088fd74ee680ed39916dd7f7f733035
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-02 19:55:17 +0530

    Replaced http with requests. Added joinRoom implementation

--- lib/services/api/api-room-membership.dart
+++ lib/services/api/api-room-membership.dart
@@ -1,26 +1,40 @@
-import 'dart:convert';
-
 import 'package:chatrooms/redux/models/room-membership.dart';
 import 'package:chatrooms/redux/models/room.dart';
 import 'package:chatrooms/services/api/api-rooms.dart';
-import 'package:http/http.dart' as http;
+import 'package:requests/requests.dart';
 
 class ApiRoomMembership {
   static final String _root = '${ApiRooms.root}/:roomId/members';
 
   String root(String roomId) => _root.replaceAll(':roomId', roomId);
 
-  Future<RoomMembership> getMembership(RoomModel room) async {
-    final String url =
-        '${root(room.id)}/byuid/5e64e8a8f7a63e19e7bfc2e8'; //username: tony_stark
+  Future<RoomMembership> getMembership(RoomModel room, String userId) async {
+    final String url = '${root(room.id)}/byuid/$userId';
 
-    http.Response response = await http.get(url);
+    Response response = await Requests.get(url);
     if (response.statusCode == 404) {
       return RoomMembership.notAMember(room);
     } else if (response.statusCode == 200) {
-      return RoomMembership.fromJson(jsonDecode(response.body));
+      return RoomMembership.fromJson(response.json());
     } else {
       return RoomMembership.undetermined;
     }
+  }
+
+  Future<RoomMembership> joinRoom(RoomModel room) async {
+    Response response = await Requests.post(root(room.id));
+
+    return RoomMembership.fromJson(response.json(), room: room);
+  }
+
+  Future<RoomMembership> leaveRoom(RoomModel room) async {
+    final String url = '${root(room.id)}/${room.membership.id}';
+    Response response = await Requests.delete(url);
+
+    if (response.success) {
+      return RoomMembership.notAMember(room);
+    }
+
+    return room.membership;
   }
 }
