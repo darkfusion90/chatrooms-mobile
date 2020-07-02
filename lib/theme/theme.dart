8be1ff54c1a7671ec725d1bac7bc681d20f4fab5
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-02 19:50:00 +0530

    Added property: backgroundColor (orangeAccent)

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -4,6 +4,7 @@ abstract class ChatRoomsTheme {
   static ThemeData get defaultTheme => ThemeData(
         primaryColor: Colors.purple,
         accentColor: Colors.orange,
+        backgroundColor: Colors.orangeAccent,
         iconTheme: IconThemeData(color: Colors.purple),
       );
 }
