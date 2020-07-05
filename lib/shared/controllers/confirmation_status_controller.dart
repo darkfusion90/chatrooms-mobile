be57a813fa0a2196668015d50783049f672e2c68
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-05 17:39:29 +0530

    Added controller: ConfirmationStatusController

---
+++ lib/shared/controllers/confirmation_status_controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:chatrooms/shared/enums/enum_confirmation_status.dart';
+
+class ConfirmationStatusController extends ValueNotifier<ConfirmationStatus> {
+  ConfirmationStatusController([ConfirmationStatus value]) : super(value);
+}
