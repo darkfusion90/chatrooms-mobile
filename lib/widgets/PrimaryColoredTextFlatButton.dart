9bdfe53de35b93d992eea2f64981087a1f3a8b99
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-05 17:23:49 +0530

    Added optional child property

--- lib/widgets/PrimaryColoredTextFlatButton.dart
+++ lib/widgets/PrimaryColoredTextFlatButton.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 
 class PrimaryColoredTextFlatButton extends StatelessWidget {
+  final Widget child;
   final String text;
   final VoidCallback onPressed;
 
   const PrimaryColoredTextFlatButton({
     this.text,
     this.onPressed,
-  });
+    this.child,
+  }) : assert(
+          text != null || child != null,
+          'You must provide either a child or a text(String)',
+        );
 
   @override
   Widget build(BuildContext context) {
     return FlatButton(
-      child: Text(
-        text ?? '',
-        style: TextStyle(color: Theme.of(context).primaryColor),
-      ),
+      textColor: Theme.of(context).primaryColor,
+      child: text == null ? child : Text(text),
       onPressed: onPressed,
       padding: EdgeInsets.all(4),
     );
