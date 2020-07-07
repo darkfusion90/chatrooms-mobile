888fc3b8288ef4f58544f893c7b55cef4755f731
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-07 20:11:39 +0530

    Implementation of action "UpdateActiveRoomMembersAction" added

--- lib/redux/actions/active_room_actions/update-active-room-members.dart
+++ lib/redux/actions/active_room_actions/update-active-room-members.dart
@@ -1,6 +1,9 @@
 import 'package:chatrooms/redux/actions/active_room_actions/set-active-room.dart';
 import 'package:chatrooms/redux/models/room-member.dart';
+import 'package:chatrooms/redux/models/room.dart';
+import 'package:chatrooms/redux/selectors/active_room_selectors/active_room_selector.dart';
 import 'package:chatrooms/redux/state/AppState.dart';
+import 'package:chatrooms/services/api/api.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 
@@ -11,6 +14,13 @@ class UpdateActiveRoomMembersAction {
 }
 
 ThunkAction<AppState> updateActiveRoomMembers([SetActiveRoomAction action]) {
-  return (Store<AppState> store) =>
-      store.dispatch(UpdateActiveRoomMembersAction([]));
+  return (Store<AppState> store) async {
+    final RoomModel activeRoom =
+        action == null ? activeRoomSelector(store.state) : action.room;
+
+    final List<RoomMember> members =
+        await Api.roomMembership.getMembers(activeRoom);
+
+    store.dispatch(UpdateActiveRoomMembersAction(members));
+  };
 }
