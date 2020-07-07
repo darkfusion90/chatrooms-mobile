d0b1f22a9ce254af7d47a075c08101366af434ed
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-07 20:51:40 +0530

    Added util: isAlphanumeric(String str)

--- lib/utils/app_utils.dart
+++ lib/utils/app_utils.dart
@@ -4,6 +4,26 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 abstract class AppUtils {
   static const bool isProductionMode = Foundation.kReleaseMode;
   static const bool isDevMode = Foundation.kDebugMode;
+
+  static bool isAlphanumeric(String str) {
+    bool _alphanumericTest(int codeUnit) {
+      final double _codeUnit = codeUnit.toDouble();
+      final Range digits = Range(48, 57);
+      final Range uppercase = Range(65, 90);
+      final Range lowercase = Range(97, 122);
+
+      return digits.contains(_codeUnit) ||
+          uppercase.contains(_codeUnit) ||
+          lowercase.contains(_codeUnit);
+    }
+
+    bool isAlphanumeric = false;
+    str.runes.forEach(
+      (int codePoint) => isAlphanumeric = _alphanumericTest(codePoint),
+    );
+
+    return isAlphanumeric;
+  }
 }
 
 abstract class AppDotEnv {
@@ -12,4 +32,13 @@ abstract class AppDotEnv {
   static String get productionHomeUrl => DotEnv().env['prod_home_url'];
 
   static String get developmentHomeUrl => DotEnv().env['dev_home_url'];
+}
+
+class Range {
+  final double from;
+  final double to;
+
+  Range(this.from, this.to);
+
+  bool contains(double val) => val >= from && val <= to;
 }
