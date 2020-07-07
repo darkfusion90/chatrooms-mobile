21e9a95c4a992e650379525910078c5dea8ebb5e
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-07 20:43:24 +0530

    Added variable size of the avatar by making the fontSize of the letter variable

--- lib/widgets/letter-avatar.dart
+++ lib/widgets/letter-avatar.dart
@@ -1,10 +1,18 @@
+import 'package:chatrooms/utils/app_utils.dart';
 import 'package:flutter/material.dart';
 
 class LetterAvatar extends StatelessWidget {
   static const String _FALLBACK_LETTER = '#';
   final String username;
+  final double size;
 
-  const LetterAvatar({this.username});
+  const LetterAvatar({this.username, this.size = 36});
+
+  factory LetterAvatar.small({String username}) =>
+      LetterAvatar(username: username, size: 12);
+
+  factory LetterAvatar.medium({String username}) =>
+      LetterAvatar(username: username, size: 24);
 
   @override
   Widget build(BuildContext context) {
@@ -13,17 +21,19 @@ class LetterAvatar extends StatelessWidget {
         decoration: BoxDecoration(shape: BoxShape.circle),
         child: Text(
           _letter,
-          style: TextStyle(fontSize: 36, fontWeight: FontWeight.w400),
+          style: TextStyle(fontSize: size, fontWeight: FontWeight.w400),
         ),
       ),
     );
   }
 
   String get _letter {
-    if (username == null || username.isEmpty) {
+    if (username == null || username.isEmpty || !_isAlphanumeric) {
       return _FALLBACK_LETTER;
     }
 
     return username[0];
   }
+
+  bool get _isAlphanumeric => AppUtils.isAlphanumeric(username[0]);
 }
