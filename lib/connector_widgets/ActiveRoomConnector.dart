50874b2ac04368e229e181ab2e9ebc3645770007
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-07 20:53:19 +0530

    ViewModel updated to wrap room, members, messages into RoomDetails

--- lib/connector_widgets/ActiveRoomConnector.dart
+++ lib/connector_widgets/ActiveRoomConnector.dart
@@ -1,15 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
 import 'package:chatrooms/redux/actions/active_room_actions/reset-active-room.dart';
 import 'package:chatrooms/redux/actions/active_room_actions/set-active-room.dart';
-import 'package:chatrooms/redux/models/room-member.dart';
-import 'package:chatrooms/redux/models/room-message.dart';
-import 'package:chatrooms/redux/selectors/active_room_selectors/active_room_selector.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:redux/redux.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-
+import 'package:chatrooms/redux/models/room_details_model.dart';
 import 'package:chatrooms/redux/models/room.dart';
+import 'package:chatrooms/redux/selectors/active_room_selectors/active_room_selector.dart';
 import 'package:chatrooms/redux/state/AppState.dart';
+
+import 'package:chatrooms/connector_widgets/RoomActionsConnector.dart';
 import 'package:chatrooms/shared/typedefs/typedef_param_widget_builder.dart';
+
+typedef void RoomCallback(RoomModel room);
 
 class ActiveRoomConnector extends StatelessWidget {
   final ParameterizedWidgetBuilder<ActiveRoomViewModel> builder;
@@ -21,36 +24,52 @@ class ActiveRoomConnector extends StatelessWidget {
     return StoreConnector<AppState, Store<AppState>>(
       converter: (Store<AppState> store) => store,
       builder: (BuildContext context, Store<AppState> store) =>
-          builder(context, _viewModel(store)),
+          RoomActionsConnector(
+        room: activeRoomSelector(store.state),
+        builder: (_, roomActions) => builder(
+          context,
+          _viewModel(store, roomActions),
+        ),
+      ),
     );
   }
 
-  ActiveRoomViewModel _viewModel(Store<AppState> store) {
-    final AppState state = store.state;
+  ActiveRoomViewModel _viewModel(
+    Store<AppState> store,
+    RoomActionsViewModel roomActions,
+  ) =>
+      ActiveRoomViewModel(
+        roomDetails: _activeRoomDetails(store),
+        roomActions: roomActions,
+        setActiveRoom: _setActiveRoom(store),
+        resetActiveRoom: _resetActiveRoom(store),
+      );
 
-    return ActiveRoomViewModel(
-      activeRoomSelector(state),
-      activeRoomMembersSelector(state),
-      activeRoomMessagesSelector(state),
-      (RoomModel room) => store.dispatch(SetActiveRoomAction(room)),
-      () => store.dispatch(ResetActiveRoomAction()),
-    );
-  }
+  RoomDetailsModel _activeRoomDetails(Store<AppState> store) =>
+      RoomDetailsModel(
+        activeRoomSelector(store.state),
+        activeRoomMembersSelector(store.state),
+        activeRoomMessagesSelector(store.state),
+      );
+
+  RoomCallback _setActiveRoom(Store<AppState> store) =>
+      (RoomModel room) => store.dispatch(SetActiveRoomAction(room));
+
+  VoidCallback _resetActiveRoom(Store<AppState> store) =>
+      () => store.dispatch(ResetActiveRoomAction());
 }
 
 class ActiveRoomViewModel {
-  final RoomModel room;
-  final List<RoomMember> roomMembers;
-  final List<RoomMessage> roomMessages;
+  final RoomDetailsModel roomDetails;
+  final RoomActionsViewModel roomActions;
 
-  final ValueChanged<RoomModel> setActiveRoom;
+  final RoomCallback setActiveRoom;
   final VoidCallback resetActiveRoom;
 
-  ActiveRoomViewModel(
-    this.room,
-    this.roomMembers,
-    this.roomMessages,
+  ActiveRoomViewModel({
+    this.roomDetails,
+    this.roomActions,
     this.setActiveRoom,
     this.resetActiveRoom,
-  );
+  });
 }
