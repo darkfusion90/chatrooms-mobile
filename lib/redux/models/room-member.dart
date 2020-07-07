b9a337451869560d3c105dfcd585aa52536a601c
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-07 20:35:06 +0530

    Replaced member "RoomMembership" with "RoomMembershipType". Also, added factory method RoomMember.fromJson

--- lib/redux/models/room-member.dart
+++ lib/redux/models/room-member.dart
@@ -1,9 +1,28 @@
 import 'package:chatrooms/redux/models/room-membership.dart';
+import 'package:chatrooms/redux/models/room.dart';
 import 'package:chatrooms/redux/models/user.dart';
+import 'package:enum_to_string/enum_to_string.dart';
 
 class RoomMember {
-  final RoomMembership membership;
+  final RoomModel room;
   final UserModel user;
+  final RoomMembershipType membershipType;
 
-  RoomMember(this.user, this.membership);
+  RoomMember(this.room, this.user, this.membershipType);
+
+  factory RoomMember.fromJson(Map<String, dynamic> json) => RoomMember(
+        RoomModel.fromJson(json['room']),
+        UserModel.fromJson(json['user']),
+        EnumToString.fromString(RoomMembershipType.values, json['memberType']),
+      );
+
+  bool get isAdmin => membershipType.isAdmin;
+
+  bool get isParticipant => membershipType.isParticipant;
+
+  @override
+  String toString() => 'RoomMember(room: $room,\n'
+      'user: $user,\n'
+      'membership: $membershipType,\n'
+      ')';
 }
