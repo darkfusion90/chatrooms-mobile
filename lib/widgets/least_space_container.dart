4bae031dc3d24c5a37b7ff83ccd4925b9028d6bb
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-07 20:46:15 +0530

    Added widget: LeastSpaceContainer - takes as little space as possible

---
+++ lib/widgets/least_space_container.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class LeastSpaceContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => SizedBox(height: 0, width: 0);
+}
