03f5e8c5ff64022a425e2749961b46f0209d27b0
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-10 21:32:03 +0530

    Refactored BaseAppBar -> StatelessAppBar

--- lib/widgets/appbars/create-account-appbar.dart
+++ lib/widgets/appbars/create-account-appbar.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:chatrooms/widgets/appbars/appbar-interface.dart';
 
-class CreateAccountAppBar extends BaseAppBar {
-  @override
-  State<StatefulWidget> createState() => _CreateAccountAppBarState();
-}
-
-class _CreateAccountAppBarState extends State<CreateAccountAppBar> {
+class CreateAccountAppBar extends StatelessAppBar {
   @override
   Widget build(BuildContext context) {
     return AppBar(title: Text('Create Account'), centerTitle: true);
