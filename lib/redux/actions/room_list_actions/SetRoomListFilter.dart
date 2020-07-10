23c299ad0e416d230e40900a00eac7a0e2232ed1
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-10 17:00:56 +0530

    Added action: resetRoomListFilter

--- lib/redux/actions/room_list_actions/SetRoomListFilter.dart
+++ lib/redux/actions/room_list_actions/SetRoomListFilter.dart
@@ -1,7 +1,14 @@
+import 'package:redux/redux.dart';
+
 import 'package:chatrooms/redux/models/room_list/room_list_filter.dart';
+import 'package:chatrooms/redux/state/AppState.dart';
+import 'package:redux_thunk/redux_thunk.dart';
 
 class SetRoomListFilterAction {
   final RoomListFilter filter;
 
   const SetRoomListFilterAction(this.filter);
 }
+
+ThunkAction<AppState> resetRoomListFilterAction() => (Store<AppState> store) =>
+    store.dispatch(SetRoomListFilterAction(RoomListFilter.noFilter));
