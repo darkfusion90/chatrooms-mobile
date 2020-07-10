6e18ed9f88f27af80c4c8cb3766d6ec0dea85269
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-10 20:09:48 +0530

    Added utility class LeaveRoomConfirmation

--- lib/connector_widgets/RoomActionsConnector.dart
+++ lib/connector_widgets/RoomActionsConnector.dart
@@ -1,11 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+
 import 'package:chatrooms/redux/actions/membership_actions/join_room.dart';
 import 'package:chatrooms/redux/actions/membership_actions/leave_room.dart';
 import 'package:chatrooms/redux/models/room.dart';
 import 'package:chatrooms/redux/state/AppState.dart';
+
+import 'package:chatrooms/widgets/dialogs/dialog_manager/DialogManager.dart';
+import 'package:chatrooms/widgets/dialogs/dialog_manager/DialogNames.dart';
+import 'package:chatrooms/widgets/dialogs/leave_room_confirmation.dart';
+
 import 'package:chatrooms/shared/typedefs/typedef_param_widget_builder.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
+import 'package:chatrooms/flushbar/flushbar_store.dart';
 
 void _doNothing() {}
 
@@ -44,4 +51,30 @@ class RoomActionsViewModel {
   final VoidCallback leaveRoom;
 
   RoomActionsViewModel(this.room, this.joinRoom, this.leaveRoom);
+}
+
+class LeaveRoomConfirmation {
+  final BuildContext context;
+
+  LeaveRoomConfirmation(this.context);
+
+  factory LeaveRoomConfirmation.of(BuildContext context) =>
+      LeaveRoomConfirmation(context);
+
+  void show(RoomActionsViewModel viewModel) {
+    final RoomModel room = viewModel.room;
+
+    if (room.type.isPrivate) {
+      DialogManager.of(context).show(
+        DialogName.leaveRoomConfirmation,
+        arguments: LeaveRoomConfirmationArguments(room: room),
+      );
+    } else {
+      FlushbarStore.leaveRoomSuccessful(
+        context,
+        room: room,
+        onUndoLeaveRoom: viewModel.joinRoom,
+      ).show(context);
+    }
+  }
 }
