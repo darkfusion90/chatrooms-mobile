753dc09bf211d5e9e44c8b1cff2a3ffa976ea73b
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-10 17:04:40 +0530

    Removed mandatory "stateful-ness" of custom AppBars when extending BaseAppBar. Added options like Stateless(ful)AppBar

--- lib/widgets/appbars/appbar-interface.dart
+++ lib/widgets/appbars/appbar-interface.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-abstract class BaseAppBar extends StatefulWidget
-  implements PreferredSizeWidget {
+abstract class StatefulAppBar extends StatefulWidget
+    implements PreferredSizeWidget {
   @override
   final Size preferredSize;
   final PreferredSizeWidget bottom;
-  
-  BaseAppBar({this.bottom})
-    : preferredSize = Size.fromHeight(
-    kToolbarHeight + (bottom?.preferredSize?.height ?? 0),
-  );
+
+  StatefulAppBar({this.bottom})
+      : preferredSize = Size.fromHeight(
+          kToolbarHeight + (bottom?.preferredSize?.height ?? 0),
+        );
+}
+
+abstract class StatelessAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  @override
+  final Size preferredSize;
+  final PreferredSizeWidget bottom;
+
+  StatelessAppBar({this.bottom})
+      : preferredSize = Size.fromHeight(
+          kToolbarHeight + (bottom?.preferredSize?.height ?? 0),
+        );
 }
