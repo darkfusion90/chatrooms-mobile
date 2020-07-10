c94696cb7825dbe91de95cd09f9298c3e15f0bc2
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-10 17:05:30 +0530

    Added flushbar -> leaveRoomSuccessful

--- lib/flushbar/flushbar_store.dart
+++ lib/flushbar/flushbar_store.dart
@@ -1,7 +1,19 @@
+import 'package:chatrooms/redux/models/room.dart';
 import 'package:flutter/material.dart';
 import 'package:flushbar/flushbar.dart';
 
 abstract class FlushbarStore {
+  static Flushbar leaveRoomSuccessful(
+    BuildContext context, {
+    RoomModel room,
+    VoidCallback onUndoLeaveRoom,
+  }) =>
+      undoActionFlushbar(
+        context,
+        message: 'Room Successfully Left: "${room.name}"',
+        onUndoButtonPressed: onUndoLeaveRoom,
+      );
+
   static final Flushbar accountCreated = successFlushbar(
     title: 'Account Created',
     message: 'Account successfully created',
@@ -12,5 +24,21 @@ abstract class FlushbarStore {
         message: message,
         icon: Icon(Icons.check_circle, color: Colors.green),
         leftBarIndicatorColor: Colors.green,
+      );
+
+  static Flushbar undoActionFlushbar(
+    BuildContext context, {
+    String message,
+    VoidCallback onUndoButtonPressed,
+  }) =>
+      Flushbar(
+        leftBarIndicatorColor: Theme.of(context).primaryColor,
+        duration: Duration(seconds: 3),
+        message: message,
+        mainButton: FlatButton(
+          textColor: Colors.white,
+          child: Text('Undo'),
+          onPressed: onUndoButtonPressed,
+        ),
       );
 }
