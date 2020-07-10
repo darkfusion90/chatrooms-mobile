3251014ab5738f1d9c6a97cb023f5024a870fee3
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-10 20:15:19 +0530

    If private, shows confirmation on attempting to leave else a snackbar with undo action

--- lib/screens/home/widgets/room_feed/widgets/room_feed_item_actions.dart
+++ lib/screens/home/widgets/room_feed/widgets/room_feed_item_actions.dart
@@ -6,9 +6,9 @@ import 'package:chatrooms/redux/models/room.dart';
 
 class RoomListItemActions extends StatelessWidget {
   final RoomModel room;
-
+  
   const RoomListItemActions({@required this.room});
-
+  
   @override
   Widget build(BuildContext context) {
     return RoomActionsConnector(
@@ -20,40 +20,43 @@ class RoomListItemActions extends StatelessWidget {
 
 class _RoomListItemActionsView extends StatelessWidget {
   final RoomActionsViewModel viewModel;
-
+  
   const _RoomListItemActionsView({@required this.viewModel})
-      : assert(viewModel != null);
-
+    : assert(viewModel != null);
+  
   @override
   Widget build(BuildContext context) {
     if (viewModel.room.isMembershipUndetermined) {
       return _loadingIndicator(context);
     }
-
+    
     return viewModel.room.isMember
-        ? _memberActions
-        : _nonMemberActions(context);
+      ? _memberActions(context)
+      : _nonMemberActions(context);
   }
-
+  
   Widget _loadingIndicator(BuildContext context) => Container(
-        width: 40,
-        child: SpinKitThreeBounce(
-          size: 20,
-          color: Theme.of(context).accentColor,
-        ),
-      );
-
+    width: 40,
+    child: SpinKitThreeBounce(
+      size: 20,
+      color: Theme.of(context).accentColor,
+    ),
+  );
+  
   Widget _nonMemberActions(BuildContext context) => OutlineButton.icon(
-        icon: Icon(Icons.add_circle),
-        label: Text('Join'),
-        textColor: Theme.of(context).primaryColor,
-        onPressed: viewModel.joinRoom,
-      );
-
-  Widget get _memberActions => OutlineButton.icon(
-        icon: Icon(Icons.exit_to_app),
-        label: Text('Leave'),
-        textColor: Colors.red,
-        onPressed: viewModel.leaveRoom,
-      );
+    icon: Icon(Icons.add_circle),
+    label: Text('Join'),
+    textColor: Theme.of(context).primaryColor,
+    onPressed: viewModel.joinRoom,
+  );
+  
+  Widget _memberActions(BuildContext context) => OutlineButton.icon(
+    icon: Icon(Icons.exit_to_app),
+    label: Text('Leave'),
+    textColor: Colors.red,
+    onPressed: () {
+      viewModel.leaveRoom();
+      LeaveRoomConfirmation.of(context).show(viewModel);
+    },
+  );
 }
