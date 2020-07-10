9249dc9cf3d7b8b967ddf27b87e5dea8543a0799
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-10 21:10:38 +0530

    Refactored to match update in selector

--- lib/redux/actions/membership_actions/update_room_memberships.dart
+++ lib/redux/actions/membership_actions/update_room_memberships.dart
@@ -1,10 +1,10 @@
-import 'package:chatrooms/redux/models/account.dart';
-import 'package:chatrooms/redux/selectors/account_selectors/account_selector.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 
+import 'package:chatrooms/redux/models/account.dart';
+import 'package:chatrooms/redux/selectors/account_selectors/account_selector.dart';
+import 'package:chatrooms/redux/selectors/room_list_selectors/all_rooms_selector.dart';
 import 'package:chatrooms/redux/models/room.dart';
-import 'package:chatrooms/redux/selectors/rooms_selectors.dart';
 import 'package:chatrooms/redux/state/AppState.dart';
 import 'package:chatrooms/services/api/api.dart' show Api;
 
@@ -20,20 +20,15 @@ Future<void> _dispatch(
   RoomModel room,
 ) async {
   final String userId = account?.user?.id ?? userIdSelector(store.state);
-  if (userId == null) {
-    print('uid null. returning');
-    return;
-  }
+  if (userId == null) return;
 
-  room.membership = await Api.roomMembership.getMembership(
-    room,
-    userId,
-  );
+  room.membership = await Api.roomMembership.getMembership(room, userId);
 
   store.dispatch(UpdateRoomMembershipAction(room));
 }
 
 ThunkAction<AppState> updateRoomMemberships([AccountModel account]) {
-  return (Store<AppState> store) => roomsSelector(store.state)
+  return (Store<AppState> store) => allRoomsSelector(store.state)
+      .rooms
       .forEach((RoomModel room) => _dispatch(store, account, room));
 }
