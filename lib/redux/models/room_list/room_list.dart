067da9263f5738f43729fafd4f089fb251f2fb3e
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-10 20:11:43 +0530

    Removed field: _noFilter Added operator implementation: '+'

--- lib/redux/models/room_list/room_list.dart
+++ lib/redux/models/room_list/room_list.dart
@@ -2,18 +2,22 @@ import 'package:chatrooms/redux/models/room.dart';
 import 'package:chatrooms/redux/models/room_list/room_list_filter.dart';
 import 'package:chatrooms/redux/models/room_list/room_list_sort_algorithms.dart';
 import 'package:chatrooms/shared/enums/enum_sort_order.dart';
+import 'package:chatrooms/shared/typedefs/typedef_sort_algorithm.dart';
 import 'package:chatrooms/utils/safe_regex.dart';
 
 typedef ListWhereClauseTest<T> = bool Function(T val);
 
 class RoomListModel {
-  static const RoomListFilter _noFilter = RoomListFilter();
   final List<RoomModel> rooms;
-  final RoomListFilter filter;
 
-  RoomListModel(this.rooms, {this.filter = _noFilter}) {
+  RoomListModel(this.rooms) {
     this.sort();
   }
+
+  RoomModel operator [](int index) => rooms[index];
+
+  RoomListModel operator +(RoomListModel other) =>
+      RoomListModel(List.from(rooms)..addAll(other.rooms));
 
   @override
   bool operator ==(other) => other is RoomListModel && rooms == other.rooms;
@@ -30,56 +34,48 @@ class RoomListModel {
 
   int get length => rooms.length;
 
-  List<RoomModel> get filtered => roomsWithFilter();
-
-  RoomModel operator [](int index) => rooms[index];
-
   void replaceMatching(RoomModel toReplace) {
     rooms.forEach((room) => room == toReplace ? toReplace : room);
   }
 
-  List<RoomModel> roomsWithFilter() {
-    if (filter == _noFilter || filter.query == null) return rooms;
+  RoomListModel applyFilter(RoomListFilter filter) {
+    if (filter == RoomListFilter.noFilter) return this;
 
     final RegExp _regex = safeRegex(filter.query, caseSensitive: false);
-    return rooms.where((room) => _regex.hasMatch(room.name)).toList();
+    return RoomListModel(
+      rooms.where((room) => _regex.hasMatch(room.name)).toList(),
+    );
   }
 
   void sort({
     RoomListSortFactor factor = RoomListSortFactor.alphabetical,
     SortOrder order = SortOrder.ascending,
-  }) {
-    switch (factor) {
-      case RoomListSortFactor.alphabetical:
-        _sortAlphabetically(order);
-        break;
-      case RoomListSortFactor.creationDate:
-        _sortByCreationDate(order);
-        break;
-      default:
-        throw UnknownRoomListSortFactorException(factor);
-    }
-  }
+  }) =>
+      rooms.sort(_getSortAlgorithm(factor, order));
+}
 
-  void _sortAlphabetically(SortOrder order) {
-    switch (order) {
-      case SortOrder.ascending:
-        rooms.sort(RoomListSortAlgorithm.alphabeticalAsc);
-        break;
-      case SortOrder.descending:
-        rooms.sort(RoomListSortAlgorithm.alphabeticalDesc);
-        break;
-    }
-  }
-
-  void _sortByCreationDate(SortOrder order) {
-    switch (order) {
-      case SortOrder.ascending:
-        rooms.sort(RoomListSortAlgorithm.creationDateAsc);
-        break;
-      case SortOrder.descending:
-        rooms.sort(RoomListSortAlgorithm.creationDateDesc);
-        break;
-    }
+SortAlgorithm<RoomModel> _getSortAlgorithm(
+  RoomListSortFactor sortFactor,
+  SortOrder order,
+) {
+  switch (order) {
+    case SortOrder.ascending:
+      return _ascendingSortAlgorithm(sortFactor);
+    case SortOrder.descending:
+      return _descendingSortAlgorithm(sortFactor);
+    default:
+      throw UnknownSortOrderException(order);
   }
 }
+
+SortAlgorithm<RoomModel> _ascendingSortAlgorithm(
+        RoomListSortFactor sortFactor) =>
+    sortFactor.isAlphabetical
+        ? RoomListSortAlgorithm.alphabeticalAsc
+        : RoomListSortAlgorithm.creationDateAsc;
+
+SortAlgorithm<RoomModel> _descendingSortAlgorithm(
+        RoomListSortFactor sortFactor) =>
+    sortFactor.isAlphabetical
+        ? RoomListSortAlgorithm.alphabeticalDesc
+        : RoomListSortAlgorithm.creationDateDesc;
