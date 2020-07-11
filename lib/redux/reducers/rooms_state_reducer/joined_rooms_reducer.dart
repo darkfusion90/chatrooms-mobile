d1d43ab77e753c2b3e7fd895957be8c1d7ed3d28
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-11 10:53:31 +0530

    Added reducer for the action SetJoinedRoomsAction

--- lib/redux/reducers/rooms_state_reducer/joined_rooms_reducer.dart
+++ lib/redux/reducers/rooms_state_reducer/joined_rooms_reducer.dart
@@ -1,8 +1,13 @@
+import 'package:chatrooms/redux/actions/room_list_actions/fetch_joined_rooms.dart';
 import 'package:redux/redux.dart';
 
 import 'package:chatrooms/redux/models/room_list/room_list.dart';
 import 'package:chatrooms/redux/reducers/rooms_state_reducer/room_list_membership_reducer.dart';
 
+RoomListModel _updateJoinedRooms(_, SetJoinedRoomsAction action) =>
+    RoomListModel(action.rooms);
+
 final Reducer<RoomListModel> joinedRoomsReducer = combineReducers([
+  TypedReducer<RoomListModel, SetJoinedRoomsAction>(_updateJoinedRooms),
   roomListMembershipReducer,
 ]);
