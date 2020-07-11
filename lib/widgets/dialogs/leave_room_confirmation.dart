149999b16119033d53ebe32c3866a98b8888dece
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-11 12:18:21 +0530

    LeaveRoomConfirmation now works for all rooms instead of only private rooms

--- lib/widgets/dialogs/leave_room_confirmation.dart
+++ lib/widgets/dialogs/leave_room_confirmation.dart
@@ -5,24 +5,27 @@ import 'package:flutter/material.dart';
 class LeaveRoomConfirmationDialog extends StatelessWidget {
   final RoomModel room;
 
-  LeaveRoomConfirmationDialog({@required this.room}) : assert(room != null) {
-    assert(
-      room.type.isPrivate,
-      'LeaveRoomConfirmationDialog should not be shown for non-private rooms',
-    );
-  }
+  LeaveRoomConfirmationDialog({@required this.room}) : assert(room != null);
 
   @override
   Widget build(BuildContext context) {
     return ConfirmationDialog(
       title: Text('Leave Room "${room.name}"?'),
-      content: Text(
+      content: _content,
+      acceptText: 'Leave Room',
+    );
+  }
+
+  Widget get _content {
+    if (room.type.isPrivate) {
+      return Text(
         'This is a private room. '
         'Leaving it will require you to send a join request the next time you wish to join the room. '
         'Are you sure you want to continue?',
-      ),
-      acceptText: 'Leave Room',
-    );
+      );
+    }
+
+    return Text('Are you sure you want leave the room ${room.name}?');
   }
 }
 
