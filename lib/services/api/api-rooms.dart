970c224ca5786c5ac262fc021719112b1c130e63
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-11 10:51:25 +0530

    Added method to retrieve joined rooms

--- lib/services/api/api-rooms.dart
+++ lib/services/api/api-rooms.dart
@@ -1,12 +1,24 @@
+import 'package:chatrooms/services/api/api-user.dart';
 import 'package:chatrooms/services/api/api.dart';
 import 'package:chatrooms/services/api/config.dart';
 import 'package:chatrooms/redux/models/room.dart';
 
 class ApiRooms {
-  static final String root = ApiConfig.createUrl('rooms');
+  static final String _rootName = 'rooms';
+  static final String root = ApiConfig.createUrl(_rootName);
 
   Future<List<RoomModel>> get rooms async {
     List<dynamic> jsonList = await Api.getJson(root);
-    return jsonList.map((json) => RoomModel.fromJson(json)).toList();
+    return _parseRoomList(jsonList);
   }
+
+  Future<List<RoomModel>> get joinedRooms async {
+    Map<String, dynamic> json = await Api.getJson('${ApiUser.root}/$_rootName');
+    List<dynamic> jsonList = json['rooms'];
+
+    return _parseRoomList(jsonList);
+  }
+
+  List<RoomModel> _parseRoomList(List<dynamic> jsonList) =>
+      jsonList.map((json) => RoomModel.fromJson(json)).toList();
 }
