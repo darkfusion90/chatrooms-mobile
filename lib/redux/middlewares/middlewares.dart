f0a3b73175233a54e221bf6004741855890fb988
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-21 14:32:56 +0530

    Added branch middleware

--- lib/redux/middlewares/middlewares.dart
+++ lib/redux/middlewares/middlewares.dart
@@ -1,14 +1,18 @@
-import 'package:chatrooms/redux/middlewares/on-account-changed.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 
 import 'package:chatrooms/redux/state/AppState.dart';
-import 'on-active-room-changed-middleware.dart';
-import 'on-room-list-changed-middleware.dart';
+import 'package:chatrooms/redux/middlewares/on-active-room-changed-middleware.dart';
+import 'package:chatrooms/redux/middlewares/on-room-list-changed-middleware.dart';
+import 'package:chatrooms/redux/middlewares/on-account-changed.dart';
+import 'package:chatrooms/redux/middlewares/on-branch-list-changed-middleware.dart';
+import 'package:chatrooms/redux/middlewares/on-current-branch-changed-middleware.dart';
 
 final List<Middleware<AppState>> storeMiddlewares = [
   thunkMiddleware,
   onRoomListChangedMiddleware,
   onActiveRoomChangedMiddleware,
+  onBranchListChangedMiddleware,
+  onCurrentBranchChangedMiddleware,
   onAccountChangedMiddleware,
 ];
