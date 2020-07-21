8ceeb0fac254d1c20c40ebaa958edcfdca4e30e6
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-21 14:41:04 +0530

    Modified ActiveRoomState to include BranchState

--- lib/redux/reducers/active_room_reducer/active-room-state-reducer.dart
+++ lib/redux/reducers/active_room_reducer/active-room-state-reducer.dart
@@ -1,14 +1,17 @@
 import 'package:redux/redux.dart';
 
 import 'package:chatrooms/redux/actions/active_room_actions/reset-active-room.dart';
+import 'package:chatrooms/redux/state/ActiveRoomState.dart';
+
 import 'package:chatrooms/redux/reducers/active_room_reducer/active-room-members-reducer.dart';
 import 'package:chatrooms/redux/reducers/active_room_reducer/active-room-messages-reducer.dart';
 import 'package:chatrooms/redux/reducers/active_room_reducer/active-room-reducer.dart';
-import 'package:chatrooms/redux/state/ActiveRoomState.dart';
+import 'package:chatrooms/redux/reducers/branch_state_reducer/branch-state-reducer.dart';
 
 ActiveRoomState _reducer(ActiveRoomState state, dynamic action) =>
     ActiveRoomState(
       room: activeRoomReducer(state.room, action),
+      branchState: branchStateReducer(state.branchState, action),
       messages: activeRoomMessagesReducer(state.messages, action),
       members: activeRoomMembersReducer(state.members, action),
     );
--- lib/redux/state/ActiveRoomState.dart
+++ lib/redux/state/ActiveRoomState.dart
@@ -1,16 +1,19 @@
 import 'package:chatrooms/redux/models/room-member.dart';
 import 'package:chatrooms/redux/models/room-message.dart';
 import 'package:chatrooms/redux/models/room.dart';
+import 'package:chatrooms/redux/state/BranchState.dart';
 import 'package:chatrooms/redux/state/state_interface.dart';
 import 'package:flutter/cupertino.dart';
 
 class ActiveRoomState extends ReduxState {
   final RoomModel room;
+  final BranchState branchState;
   final List<RoomMessage> messages;
   final List<RoomMember> members;
 
   ActiveRoomState({
     @required this.room,
+    @required this.branchState,
     @required this.messages,
     @required this.members,
   })  : assert(messages != null),
@@ -18,6 +21,7 @@ class ActiveRoomState extends ReduxState {
 
   factory ActiveRoomState.init() => ActiveRoomState(
         room: null,
+        branchState: BranchState.init(),
         members: [],
         messages: [],
       );
@@ -26,12 +30,16 @@ class ActiveRoomState extends ReduxState {
   bool compareState(other) {
     return other is ActiveRoomState &&
         other.room == room &&
+        other.branchState == branchState &&
         other.messages == messages &&
         other.members == members;
   }
 
   @override
   int getHashCode() {
-    return room.hashCode ^ messages.hashCode ^ members.hashCode;
+    return room.hashCode ^
+        branchState.hashCode ^
+        messages.hashCode ^
+        members.hashCode;
   }
 }
