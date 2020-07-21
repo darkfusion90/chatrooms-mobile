112a2d798d8f1644c35c1c931e4cc21cfecf892d
darkfusion90 <gauravsharmaa341@gmail.com>
2020-07-21 14:56:58 +0530

    Added branch details to RoomDetailsModel

--- lib/redux/models/room_details_model.dart
+++ lib/redux/models/room_details_model.dart
@@ -2,10 +2,20 @@ import 'package:chatrooms/redux/models/room-member.dart';
 import 'package:chatrooms/redux/models/room-message.dart';
 import 'package:chatrooms/redux/models/room.dart';
 
+import 'branch.dart';
+
 class RoomDetailsModel {
   final RoomModel room;
+  final List<BranchModel> branches;
+  final BranchModel currentBranch;
   final List<RoomMember> members;
   final List<RoomMessage> messages;
 
-  RoomDetailsModel(this.room, this.members, this.messages);
+  RoomDetailsModel({
+    this.room,
+    this.branches,
+    this.currentBranch,
+    this.members,
+    this.messages,
+  });
 }
